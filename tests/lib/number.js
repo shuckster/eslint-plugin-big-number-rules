67280c3e33e78f33afdba53bab6f2f61335d8dbd
Conan Theobald <github@conans.co.uk>
2021-04-24 19:42:33 +0100

    Tests for toPrecision, toExponential, toString

--- tests/lib/number.js
+++ tests/lib/number.js
@@ -23,6 +23,58 @@ const tests = [
     output: 'BigNumber(one()).decimalPlaces(4);',
     errors: expectingErrors(1)
   },
+
+  {
+    code: '(1).toExponential(2);',
+    output: 'BigNumber(1).toExponential(2);',
+    errors: expectingErrors(1)
+  },
+  {
+    code: 'one.toExponential(3);',
+    output: 'BigNumber(one).toExponential(3);',
+    errors: expectingErrors(1)
+  },
+  {
+    code: 'one().toExponential(4);',
+    output: 'BigNumber(one()).toExponential(4);',
+    errors: expectingErrors(1)
+  },
+
+  {
+    code: '(1).toPrecision(2);',
+    output: 'BigNumber(1).toPrecision(2);',
+    errors: expectingErrors(1)
+  },
+  {
+    code: 'one.toPrecision(3);',
+    output: 'BigNumber(one).toPrecision(3);',
+    errors: expectingErrors(1)
+  },
+  {
+    code: 'one().toPrecision(4);',
+    output: 'BigNumber(one()).toPrecision(4);',
+    errors: expectingErrors(1)
+  },
+
+  {
+    code: '(1).toString(2);',
+    output: 'BigNumber(1).toString(2);',
+    errors: expectingErrors(1)
+  },
+  {
+    code: 'one.toString(3);',
+    output: 'BigNumber(one).toString(3);',
+    errors: expectingErrors(1)
+  },
+  {
+    code: 'one().toString(4);',
+    output: 'BigNumber(one()).toString(4);',
+    errors: expectingErrors(1)
+  },
+
+  //
+  // Number.static.*
+  //
   {
     code: 'Number.parseFloat("1.2");',
     output: 'BigNumber("1.2");',
